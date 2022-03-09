c3729f4112883da413be55b5f9d22f3b7d561b27
anlum-1936128 <56006060+anlum-1936128@users.noreply.github.com>
2022-03-09 14:48:20 -0800

    added average_profits visualizations

---
+++ average_profits.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+sns.set()
+
+
+def avg_profit(data, production):
+    data['profit'] = data['gross'] - data['budget']
+    prod_data = data[[production, 'profit']]
+    prod_data = prod_data.groupby([production]).mean()
+    prod_data.reset_index(inplace = True)
+    # not reading after groupby
+    prod_data = prod_data.sort_values(by='profit', ascending=False)
+    prod_data = prod_data.head(10)
+
+    sns.catplot(x=production, y="profit", data=prod_data, kind="bar")
+
+    production_label = production.capitalize()
+
+    plt.title("Average Profit for Each " + production_label)
+    plt.xlabel(production_label)
+    plt.xticks(rotation = 90)
+    plt.ylabel('Profit ($)')
+    # plt.yticks(np.arange(2, 1000000))
+    plt.ylim(bottom=100000)
+
+    plt.savefig((production + '_avg_profit.png'), bbox_inches='tight')
+
+
+def main():
+    data = pd.read_csv("/home/movies.csv")
+    avg_profit(data, "company")
+    avg_profit(data, "director")
+    avg_profit(data, "writer")
+
+
+if __name__ == '__main__':
+    main()
