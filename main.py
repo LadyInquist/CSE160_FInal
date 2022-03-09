0f37e097229e12954a5e22dce87eb2922c51e283
anlum-1936128 <56006060+anlum-1936128@users.noreply.github.com>
2022-03-09 14:50:33 -0800

    Update and rename main (1).py to main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import seaborn as sns
 from rating_model import ml_regressor
+from average_profit import avg_profit
 
 def main():
     data = pd.read_csv("movies.csv")
     data = data.dropna()
     ml_regressor(data)
+    # testing visualizations (problem 2)
+    avg_profit(data, "company")
+    avg_profit(data, "director")
+    avg_profit(data, "writer")
     
     print("testing part 2")
 
