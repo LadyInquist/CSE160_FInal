f5a3892452f7247e18fe7abd7aca081bf0dcd9eb
anlum-1936128 <56006060+anlum-1936128@users.noreply.github.com>
2022-03-13 21:38:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import seaborn as sns
 from rating_model import ml_regressor
-from average_profits import avg_profit
+from research_question_2
 
 def main():
     data = pd.read_csv("movies.csv")
     data = data.dropna()
     ml_regressor(data)
-    # testing visualizations (problem 2)
+
+    # calling visualizations for research question 2
     avg_profit(data, "company")
     avg_profit(data, "director")
     avg_profit(data, "writer")
+    profits_by_year(data, "company")
+    profits_by_year(data, "director")
+    profits_by_year(data, "writer")
     
     print("testing part 2")
 
